52e470eca0b946919e35bd1cb671bd207930eb77
veedanny <vinittasyvia3@gmail.com>
2024-07-09 23:01:49 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution in this file!
+let employee = {
+    name: 'John Doe',
+    streetAddress: '123 Main St'
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value
+    };
+}
+let updatedEmployee = updateEmployeeWithKeyAndValue(employee, 'name', 'Jane Doe');
+console.log(updatedEmployee);
+// Output: { name: 'Jane Doe', streetAddress: '123 Main St' }
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+destructivelyUpdateEmployeeWithKeyAndValue(employee, 'name', 'Jane Doe');
+console.log(employee);
+// Output: { name: 'Jane Doe', streetAddress: '123 Main St' }
+function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+}
+let newEmployee = deleteFromEmployeeByKey(employee, 'streetAddress');
+console.log(newEmployee);
+// Output: { name: 'John Doe' }
+console.log(employee);
+// Output: { name: 'John Doe', streetAddress: '123 Main St' } (original employee Object remains unchanged)
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
+destructivelyDeleteFromEmployeeByKey(employee, 'streetAddress');
+console.log(employee);
+// Output: { name: 'John Doe' }
+
